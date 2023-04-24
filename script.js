7eb0ab3c301cabc119411d7dec9fab4188d92ba5
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 20:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let level = document.querySelectorAll("ul>li")
+level.addEventListner("click",()=>{
+	for(let i=0;i<level.length;i++) {
+		if(level[i].id=="level"){
+			window.alert('the level of the element is: '+(i+1))
+		
+		 return;
+		}
+	}
+})
